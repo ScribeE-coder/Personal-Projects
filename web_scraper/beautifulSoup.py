c4c58db05517b889942de77de4fab60fd9bac030
ScribeE-coder <kidmusician006@gmail.com>
2025-12-12 22:57:23 -0800

    creating functions for scraping titles

--- web_scraper/beautifulSoup.py
+++ web_scraper/beautifulSoup.py
@@ -4,5 +4,26 @@ from bs4 import BeautifulSoup
 with open("dummy_html.html", "r") as file: 
     doc = BeautifulSoup(file, "html.parser")
 
-title = doc.title
-print(title)
+
+def find_title(document): 
+    title = doc.title
+    return title 
+
+title = find_title(doc)
+
+def title_listy(title): 
+    words = []
+    for word in title: 
+        words.append(word)
+    return words
+
+def title_to_str(words): 
+    stringy = "" 
+    for word in words: 
+        stringy += word 
+    return stringy 
+
+word_listy = title_listy(title)
+stringy_title = title_to_str(word_listy)
+print(stringy_title) 
+
