b3047effcde77e1ac975e759170d3d18ad3c8750
ScribeE-coder <kidmusician006@gmail.com>
2025-12-29 16:56:32 -0800

    got everything up and running with the typical hello world

---
+++ learningCSharp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System; 
+
+namespace HelloWorldApp
+{
+    class Program
+    {
+        static void Main(string[] agrs)
+        {
+            Console.WriteLine("Hello, World! Everyone starts off with this smh.");
+            Console.WriteLine("This is so much like Java I am terrified."); 
+        }
+    }
+}
