c57dbb8a04b12aba45bb1edbededfc9663d4a05a
ScribeE-coder <kidmusician006@gmail.com>
2025-12-29 17:58:43 -0800

    playing around with functions

--- learningCSharp/Program.cs
+++ learningCSharp/Program.cs
@@ -1,18 +1,38 @@
 ﻿using System; 
 
-namespace HelloWorldApp
+namespace learningCSharp
 {
-    class Program
+    class Line
     {
         // similar to Java's main method 
         static void Main(string[] agrs)
         {  
-            // similar to Python's print 
+            // similar to Python's print or Java's System.out.println
             Console.WriteLine("Hello, World! Everyone starts off with this smh.");
             Console.WriteLine("This is so much like Java I am terrified."); 
             
+            int num = 400; 
+            Console.WriteLine("Here is an integer: " + num); 
+
+            int num2 = 200; 
+            Console.WriteLine("Their total is: " + (num + num2)); 
+
+            // identical to for loops in Java 
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("The current value of i is: " + i); 
+            }
+
+            string name = "Chrystopher"; 
+            for (char c = name[0]; c <= name[name.Length - 1]; c++)
+            {
+                Console.WriteLine("The current character is: " + c); 
+            }  
+            
             // Waits for user input before closing window 
             Console.ReadLine(); 
+
+
         }
     }
 }
